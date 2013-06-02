af335146a56e86700e3ea66956045c8c173cfbd0
Oliver Gould <ver@twitter.com>
2013-06-02 07:51:27 +0000

    clearer scoring

--- src/letterdepress/core.clj
+++ src/letterdepress/core.clj
@@ -3,11 +3,6 @@
   (:use [clojure.java.io])
   (:gen-class))
 
-; define a board type with:
-; - open keys
-; - claimed keys
-; - their available keys
-
 (defn- compare-freqs
   [word-freq board-freq]
   (into {}
@@ -20,26 +15,28 @@
 
 (defn score
   [word two-pointers one-pointers]
-  (let [state (reduce
-          (fn [state c]
-            (let [points (state :points 0)
-                  tps (state :two-pointers {})
-                  ops (state :one-pointers {})
-                  tp  (tps c 0)
-                  op  (ops c 0)]
-              (cond
-                (> tp 0) (assoc state
-                  :two-pointers (assoc tps c (- tp 1))
-                  :points       (+ points 2))
-                (> op 0) (assoc state
-                  :one-pointers (assoc ops c (- op 1))
-                  :points       (+ points 1))
-                :else state)))
-          (assoc {}
-            :two-pointers two-pointers
-            :one-pointers one-pointers)
-          word)]
-    (state :points 0)))
+  (reduce
+    (fn [state c]
+      (let [won-points  (state :won-points  0)
+            lost-points (state :lost-points 0)
+            tps (state :two-pointers {})
+            ops (state :one-pointers {})
+            tp  (tps c 0)
+            op  (ops c 0)]
+        (cond
+          (> tp 0) (assoc state
+            :two-pointers (assoc tps c (- tp 1))
+            :won-points   (inc won-points)
+            :lost-points  (inc lost-points))
+          (> op 0) (assoc state
+            :one-pointers (assoc ops c (- op 1))
+            :won-points   (inc won-points))
+          :else state)))
+    (assoc {}
+      :word         word
+      :two-pointers two-pointers
+      :one-pointers one-pointers)
+    word))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -57,9 +54,10 @@
           un-played-freq (frequencies un-played)
           in-play-freq   (frequencies in-play)
           score (fn [word]
-            [word (score word un-played-freq in-play-freq)])
-          print-score (fn [[word score]]
-            (println (format "%d %s" score word)))]
+            (score word un-played-freq in-play-freq))
+          print-score (fn [score]
+            (println
+              (format "+%d -%d %s" (score :won-points 0) (score :lost-points 0) (score :word))))]
       (with-open [rdr (reader "/usr/share/dict/words")]
         (doseq [word (map #(.toLowerCase %) (line-seq rdr))
                :when (on-board word)]
