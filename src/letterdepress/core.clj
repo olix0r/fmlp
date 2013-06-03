e70bb376219b704e6bea10974104efcd6b42015f
Oliver Gould <ver@twitter.com>
2013-06-03 15:12:50 +0000

    cleanup, with better arg parsing and high score calculation

--- src/letterdepress/core.clj
+++ src/letterdepress/core.clj
@@ -1,77 +1,153 @@
 (ns letterdepress.core
   (:gen-class)
   (:use [clojure.core.reducers :only (cat)])
+  (:use [clojure.set])
   (:use [clojure.java.io]))
-
-(defn- availability
-  [word board]
-  (into {}
-    (for [c (keys word)]
-      [c (- (board c 0) (word c))])))
-
-(defn- available?
-  [freqs]
-  (reduce
-    (fn [ok [_ avail]]
-      (and ok (>= avail 0)))
-    true
-    freqs))
 
 (defn- update-score
   [letter
-   {won       :won,
-    lost      :lost,
-    in-play   :in-play,
-    un-played :un-played,
-    nullified :nullified,
+   {won      :won,
+    lost     :lost,
+    opponent :opponent,
+    open     :open,
+    occupied :occupied,
     :as state
     :or {won 0 lost 0}}]
-  (let [i (in-play   letter 0)
-        u (un-played letter 0)
-        n (nullified letter 0)]
+  (let [unc (open     letter 0)
+        opp (opponent letter 0)
+        occ (occupied letter 0)]
     (into state (cond
-      (> i 0) {:in-play   (assoc in-play letter (dec i))
-               :lost      (inc lost)
-               :won       (inc won)}
-      (> u 0) {:un-played (assoc un-played letter (dec u))
-               :won       (inc won)}
-      (> n 0) {:nullified (assoc nullified letter (dec n))}
-      :else   {:valid     false}))))
+      (> unc 0)
+        {:open     (assoc open     letter (dec unc))
+         :won      (inc won)}
+      (> opp 0)
+        {:opponent (assoc opponent letter (dec opp))
+         :lost     (inc lost)
+         :won      (inc won)}
+      (> occ 0)
+        {:occupied (assoc occupied letter (dec occ))}
+      :else
+        {:valid    false}))))
 
 (defn score
-  [word in-play un-played nullified]
-  (loop [current-score {:in-play   in-play
-                        :un-played un-played
-                        :nullified nullified
-                        :valid     true}
-         letters word]
-    (if (and (not-empty letters) (current-score :valid))
-      (recur (update-score (first letters) current-score) (rest letters))
-      current-score)))
-
-(defn print-score
   [word
-   {won   :won,
-    lost  :lost,
-    valid :valid,
-    :or {won 0 lost 0 valid false}}]
-  (if valid
-    (println (format "+%d -%d %s" won lost word))))
+   {open     :open,
+    opponent :opponent,
+    occupied :occupied,
+    :or {open {}
+         opponent {}
+         occupied {}}}]
+  (loop [letters  word
+         score    {:word     word
+                   :open     open
+                   :opponent opponent
+                   :occupied occupied
+                   :valid    true}]
+    (if (and (not-empty letters) (score :valid))
+      (recur (rest letters) (update-score (first letters) score))
+      score)))
 
-(def dict (resource "words"))
+(defn select-high-scores
+  [n scores]
+  (flatten (vals (reduce
+    (fn [high-scores
+         {points :won :as score :or {won 0}}]
+      (if-let [s (high-scores points)]
+        (assoc high-scores points (conj s score))
+        (if (< (count high-scores) n)
+          (assoc high-scores points [score])
+          (let [lowest (key (first high-scores))]
+            (if (> points lowest)
+              (assoc (dissoc high-scores lowest) points [score])
+              high-scores)))))
+    (sorted-map)
+    scores))))
+
+(defn filter-valid
+  [scores]
+  (filter #(% :valid false) scores))
+
+(defn format-score
+  [{won :won, lost :lost, word :word, :or {won 0 lost 0} :as score}]
+  (format "+%02d -%02d %s" won lost word))
+
+(defn format-scores
+  [scores]
+  (map format-score scores))
+
+(defn- warn
+  [config warning]
+  (assoc config :warnings (conj (config :warnings) warning)))
+
+(defn- getopt
+  [args config opt-parse]
+  (loop [args        args
+         config      config]
+    (if-let [arg (first args)]
+      (recur (rest args)
+        (if (= (first arg) \-)
+          (reduce opt-parse config (rest arg))
+          (assoc config :args (conj (config :args) arg))))
+      config)))
+
+(defn- parse-config
+  ([args]
+    (parse-config args {}))
+
+  ([args config]
+    (defn- parse-opts
+      [c flag]
+      (case flag
+        \H (assoc c :high-scores (inc (c :high-scores 0)))
+        \h (assoc c :do-help? true)
+        (warn c "Unexpected flag: %s" flag)))
+    (let [config  (getopt args config parse-opts)]
+      (loop [args (config :args [])
+             positions [:open :opponent :occupied]
+             config config]
+        (if (or (empty? args) (empty? positions))
+          config
+          (recur (rest args) (rest positions) (assoc config
+            (first positions) (frequencies (.toLowerCase (first args))))))))))
 
 (defn -main
-  "Letterpress got you sad?  This will make you sadder.
-  "
-  ([un-played]
-    (-main un-played ""))
-  ([un-played in-play]
-    (-main un-played in-play ""))
-  ([un-played in-play nullified]
-    (let [un-played (frequencies (.toLowerCase un-played))
-          in-play   (frequencies (.toLowerCase in-play))
-          nullified (frequencies (.toLowerCase nullified))]
-      (with-open [dict (reader dict)]
-        (doseq [word (line-seq dict)]
-          (print-score word
-            (score (.toLowerCase word) in-play un-played nullified)))))))
+"Letterpress got you sad?  This will make you sadder.
+  fmlp open-letters [opponent-letters [occupied-letters]]"
+  [& args]
+  (let [{open        :open,
+         opponent    :opponent,
+         occupied    :occupied,
+         warnings    :warnings,
+         do-help?    :do-help?,
+         high-scores :high-scores,
+         :as config
+         :or {open     {}
+              opponent {}
+              occupied {}
+              do-help? false
+              high-scores 0}}
+                  (parse-config args {})
+
+        help      (:doc (meta (var -main)))
+
+        is-empty? (empty? (flatten [open opponent occupied]))
+
+        select-scores
+                  (if (> high-scores 0)
+                    #(select-high-scores high-scores %)
+                    identity)
+
+        scores    (fn [words]
+                    (select-scores (filter-valid
+                      (map #(score (.toLowerCase %) config) words))))]
+
+    (doseq [warning warnings]
+      (.println *err* warning))
+
+    (cond
+      do-help?  (println help)
+      is-empty? (.println *err* help)
+      :else
+        (with-open [dict (reader (resource "words"))]
+          (doseq [score (format-scores (scores (line-seq dict)))]
+            (println score))))))
