6404f2ccc92be1ac14cac4f99ca68fd08fc54d4c
Oliver Gould <ver@twitter.com>
2013-06-05 15:07:26 +0000

    general cleanup; support the -o flag to limit the results to game-enders

--- src/fmlp/core.clj
+++ src/fmlp/core.clj
@@ -1,8 +1,12 @@
-:13(ns fmlp.core
+(ns fmlp.core
   (:gen-class)
   (:use [clojure.core.reducers :only (cat)])
-  (:use [clojure.set])
   (:use [clojure.java.io]))
+
+(defn check-game-over
+  [{open :open, :as score :or {open {}}}]
+    (assoc score :game-over?
+      (zero? (reduce + 0 (flatten (vals open))))))
 
 (defn- update-score
   [letter
@@ -13,38 +17,26 @@
     occupied :occupied,
     :as state
     :or {points 0 taken 0}}]
-    (let [unc (open     letter 0)
-          opp (opponent letter 0)
-          occ (occupied letter 0)]
-      (into state (cond
-        (pos? unc)
-          {:open     (assoc open     letter (dec unc))
-           :points   (inc points)}
-        (pos? opp)
-          {:opponent (assoc opponent letter (dec opp))
-           :taken    (inc taken)
-           :points   (inc points)}
-        (pos? occ)
-          {:occupied (assoc occupied letter (dec occ))}
-        :else
-          {:valid    false}))))
-
-(defn check-game-over
-  [score]
-    (assoc score :game-over (zero? (reduce + 0 (flatten (vals (score :open)))))))
-
-(defn check-game-overs
-  [scores]
-    (map check-game-over scores))
+    (check-game-over
+      (into state
+        (let [unc (open     letter 0)
+              opp (opponent letter 0)
+              occ (occupied letter 0)]
+          (cond
+            (pos? unc)
+              {:open     (assoc open     letter (dec unc))
+               :points   (inc points)}
+            (pos? opp)
+              {:opponent (assoc opponent letter (dec opp))
+               :taken    (inc taken)
+               :points   (inc points)}
+            (pos? occ)
+              {:occupied (assoc occupied letter (dec occ))}
+            :else
+              {:valid    false})))))
 
 (defn score
-  [word
-   {open     :open,
-    opponent :opponent,
-    occupied :occupied,
-    :or {open {}
-         opponent {}
-         occupied {}}}]
+  [word open opponent occupied]
     (loop [letters  word
            score    {:word     word
                      :open     open
@@ -79,21 +71,15 @@
       (fn [s] (+ (s :points 0) (s :taken 0)))
       scores))
 
-(defn is-valid
-  [score]
-    (score :valid false))
+(defn is-valid [{v :valid :or {v false}}] v)
 
 (defn format-score
-  [{points  :points,
-    taken   :taken,
-    word    :word,
-    game-over? :game-over,
-    :or {points 0 taken 0} :as score}]
+  [{points     :points,
+    taken      :taken,
+    word       :word,
+    game-over? :game-over?,
+    :or {points 0 taken 0 game-over? false}}]
     (format "%s +%02d -%02d %s"(if game-over? "*" " ") points taken word))
-
-(defn format-scores
-  [scores]
-    (map format-score scores))
 
 (defn- warn
   [config warning]
@@ -112,14 +98,25 @@
             (assoc config :args (conj (config :args) arg))))
         config)))
 
+(def default-config
+  { :high-scores     0
+    :only-game-over? false
+    :do-help?        false
+    :open            {}
+    :opponent        {}
+    :occupied        {}
+    :warnings        []})
+
 (defn- parse-config
-  ([args] (parse-config args {}))
+  ([args]
+    (parse-config args default-config))
   ([args config]
     (defn- opts
       [c flag]
       (case flag
         \H (assoc c :high-scores (inc (c :high-scores 0)))
         \h (assoc c :do-help? true)
+        \o (assoc c :only-game-over? true)
         (warn c (format "Unexpected flag: %s" flag))))
     (let [config  (getopt args config opts)]
       (loop [args (config :args [])
@@ -127,53 +124,50 @@
              config config]
         (if (or (empty? args) (empty? positions))
           config
-          (recur (rest args) (rest positions) (assoc config
-            (first positions) (frequencies (.toLowerCase (first args))))))))))
+          (recur (rest args) (rest positions)
+            (assoc config (first positions)
+              (frequencies (.toLowerCase (first args))))))))))
 
-(def board-capacity (* 5 5))
+(def board-sz (* 5 5))
 
 (defn -main
 "Letterpress got you sad?  This will make you sadder.
   fmlp open-letters [opponent-letters [occupied-letters]]"
   [& args]
-    (let [{open        :open,
-           opponent    :opponent,
-           occupied    :occupied,
-           warnings    :warnings,
-           do-help?    :do-help?,
-           high-scores :high-scores,
-           :as config
-           :or {open     {}
-                opponent {}
-                occupied {}
-                warnings []
-                do-help? false
-                high-scores 0}}
-                    (parse-config args {})
+    (let [{open            :open,
+           opponent        :opponent,
+           occupied        :occupied,
+           warnings        :warnings,
+           do-help?        :do-help?,
+           high-scores     :high-scores,
+           only-game-over? :only-game-over?}
+                    (parse-config args)
 
           help-msg  (:doc (meta (var -main)))
 
-          board-sz  (reduce + 0 (flatten (map vals (filter not-empty [open opponent occupied]))))
+          letter-count
+                    (reduce + 0 (flatten (map vals (filter not-empty [open opponent occupied]))))
 
           select-scores
-                    (if (> high-scores 0)
-                      #(select-high-scores high-scores %)
-                      identity)
+                    (cond
+                      only-game-over?   #(filter (fn [s] (s :game-over? false)) %)
+                      (> high-scores 0) #(select-high-scores high-scores %)
+                      :else identity)
 
-          scores    (fn [words]
-                      (map check-game-over
-                        (select-scores
-                          (filter is-valid
-                            (map #(score (.toLowerCase %) config)
-                              words)))))]
+          scores        (fn [words]
+                          (select-scores
+                            (filter is-valid
+                              (map #(score (.toLowerCase %) open opponent occupied)
+                                words))))]
 
       (doseq [w warnings]
         (.println *err* w))
 
-      (if (> board-sz board-capacity)
-        (.println *err* (format "%d too many letters provided" (- board-sz board-capacity)))
-        (if (< board-sz board-capacity)
-          (.println *err* (format "%d too few letters provided" (- board-capacity board-sz)))))
+      (cond
+        (> letter-count board-sz)
+          (.println *err* (format "%d too many letters provided" (- letter-count board-sz)))
+        (< letter-count board-sz)
+          (.println *err* (format "%d too few letters provided" (- board-sz letter-count))))
 
       (cond
         do-help?
@@ -182,5 +176,5 @@
           (.println *err* help-msg)
         :else
           (with-open [dict (reader (resource "words"))]
-            (doseq [s (format-scores (scores (line-seq dict)))]
+            (doseq [s (map format-score (scores (line-seq dict)))]
               (println s))))))
